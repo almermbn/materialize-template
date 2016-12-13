79abaf3540ed6cf25b71b85cec6c5cc561c40020
Almer Nakano <almer.nakano@gmail.com>
2016-12-13 02:24:30 -0200

    revision_001

--- home.controller.js
+++ home.controller.js
@@ -9,7 +9,19 @@
    function HomeController()
    {
       var hc = this;
-      hc.name = '';
+      hc.play = true;
+
+      hc.name = 'asdasd';
+
+      hc.muteVideo = function(play){
+        if(play){
+            $("video").prop('muted', play);
+        } else {
+            $("video").prop('muted', play);
+        }
+
+        hc.play = !hc.play;
+      }
     
    }
 })();
